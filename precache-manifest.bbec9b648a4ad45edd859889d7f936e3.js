9a31afefd5dd05bd9d47754cf3da922694ac5271
mordak-95 <msroshanbin@gmail.com>
2020-09-26 21:56:56 +0330

    Updates

---
+++ precache-manifest.bbec9b648a4ad45edd859889d7f936e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c30ac7bde8295ff4088a3fb1c35666",
+    "url": "/neosign-app/index.html"
+  },
+  {
+    "revision": "f238672234ee1931f4bb",
+    "url": "/neosign-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f75354e29d4a90de5118",
+    "url": "/neosign-app/static/js/2.d555b863.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neosign-app/static/js/2.d555b863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f238672234ee1931f4bb",
+    "url": "/neosign-app/static/js/main.11f195f1.chunk.js"
+  },
+  {
+    "revision": "9688991f6398aab3157e",
+    "url": "/neosign-app/static/js/runtime-main.c8fd5851.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/neosign-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
