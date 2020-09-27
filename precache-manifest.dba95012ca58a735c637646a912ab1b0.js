20854d1de358d50542d13eca37744c65ea514b02
mordak-95 <msroshanbin@gmail.com>
2020-09-27 22:03:08 +0330

    Updates

---
+++ precache-manifest.dba95012ca58a735c637646a912ab1b0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a1f3f0381a04d9dff712ca04b1ed1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dd699c1c86af3f35677",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "7547306ec9a242b8d717",
+    "url": "/static/css/main.c5f7fb28.chunk.css"
+  },
+  {
+    "revision": "6dd699c1c86af3f35677",
+    "url": "/static/js/2.03164fcf.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.03164fcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7547306ec9a242b8d717",
+    "url": "/static/js/main.398fb50e.chunk.js"
+  },
+  {
+    "revision": "9df312c990a01bf9e75d",
+    "url": "/static/js/runtime-main.e27612a3.js"
+  },
+  {
+    "revision": "d4a07ed205a89bfecfcbac5ead8b1950",
+    "url": "/static/media/bg.d4a07ed2.jpg"
+  },
+  {
+    "revision": "295a6a5bf993a82fa075dcfb92dd8d74",
+    "url": "/static/media/london.295a6a5b.jpg"
+  },
+  {
+    "revision": "9b8aec47461c9520255a73f668f280d8",
+    "url": "/static/media/new-york.9b8aec47.jpg"
+  },
+  {
+    "revision": "570f6b1f35f60c4894a57de87af0fcec",
+    "url": "/static/media/portrait.570f6b1f.jpg"
+  },
+  {
+    "revision": "f6fb09643d3447e810fbdd4139284446",
+    "url": "/static/media/rome.f6fb0964.jpg"
+  },
+  {
+    "revision": "29ebcf74577c7d0aa77b3fb215246b40",
+    "url": "/static/media/tokyo.29ebcf74.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/neosign-app/precache-manifest.2d8d06194f1358d96af264721bd541d6.js"
+  "/precache-manifest.dba95012ca58a735c637646a912ab1b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/neosign-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
